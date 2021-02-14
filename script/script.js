f9ff5de0ad10930f9d8c4900dfc0d9807104f3ce
ElvinaZykrina <71496756+ElvinaZykrina@users.noreply.github.com>
2021-02-14 09:39:44 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,8 @@
+ $(document).ready(function(){
+	$('button.button-card').click(function () {
+		$(".modal").show();
+		});
+	$('button.close').click(function () {
+		$(".modal").hide();
+		});
+	});
